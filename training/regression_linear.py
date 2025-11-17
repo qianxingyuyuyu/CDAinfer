97070326bc4569e10aa7d5370004acde53b646ee
qianxingyuyuyu <1686667118@qq.com>
2025-11-17 20:33:51 +0800

    Add files via upload

---
+++ training/regression_linear.py
@@ -0,0 +1,162 @@
+import tensorflow as tf
+from keras.layers import Input, LSTM, Concatenate, Dense,Dropout,Masking, BatchNormalization
+from keras.optimizers import Adam
+import json
+import random
+from sklearn.model_selection import train_test_split
+import numpy as np
+from utils import padding,generate_data
+
+data = []
+count=0
+data_path='../data_processing/newnewdata_nn.jsonl'
+with open(data_path, 'r') as file:
+    for line in file:
+        try:
+            l=json.loads(line)
+            if l['behavior']<300:
+                    count += 1
+                    data.append(l)
+        except:
+            print('error')
+print(count)
+
+
+np.random.seed(42)
+random.seed(42)  
+train_data, other_data= train_test_split(data, test_size=0.2,random_state=42)  
+val_data, test_data = train_test_split(other_data, test_size=0.5,random_state=42) 
+
+
+X_train_history,X_val_history,X_test_history=generate_data('history',train_data,val_data, test_data)
+X_train_bids,X_val_bids,X_test_bids=generate_data('bids',train_data,val_data, test_data)
+X_train_asks,X_val_asks,X_test_asks=generate_data('asks',train_data,val_data, test_data)
+X_train_unit,X_val_unit,X_test_unit=generate_data('unit',train_data,val_data, test_data)
+X_train_previous_offer,X_val_previous_offer,X_test_previous_offer=generate_data('previous_offer',train_data,val_data, test_data)
+X_train_previous_trade,X_val_previous_trade,X_test_previous_trade=generate_data('previous_trade',train_data,val_data, test_data)
+X_train_trade_count,X_val_trade_count,X_test_trade_count=generate_data('trade_count',train_data,val_data, test_data)
+X_train_trade_max,X_val_trade_max,X_test_trade_max=generate_data('trade_max',train_data,val_data, test_data)
+X_train_trade_min,X_val_trade_min,X_test_trade_min=generate_data('trade_min',train_data,val_data, test_data)
+X_train_trade_avg,X_val_trade_avg,X_test_trade_avg=generate_data('trade_avg',train_data,val_data, test_data)
+X_train_trade_mid,X_val_trade_mid,X_test_trade_mid=generate_data('trade_mid',train_data,val_data, test_data)
+X_train_offer_count,X_val_offer_count,X_test_offer_count=generate_data('offer_count',train_data,val_data, test_data)
+X_train_offer_max,X_val_offer_max,X_test_offer_max=generate_data('offer_max',train_data,val_data, test_data)
+X_train_offer_min,X_val_offer_min,X_test_offer_min=generate_data('offer_min',train_data,val_data, test_data)
+X_train_offer_avg,X_val_offer_avg,X_test_offer_avg=generate_data('offer_avg',train_data,val_data, test_data)
+X_train_offer_mid,X_val_offer_mid,X_test_offer_mid=generate_data('offer_mid',train_data,val_data, test_data)
+
+X_train_role,X_val_role,X_test_role=generate_data('role',train_data,val_data, test_data)
+
+y_regression_train,y_regression_val,y_regression_test=generate_data('behavior',train_data,val_data, test_data)
+
+
+input_history = Input(shape=(5, ))
+input_bids = Input(shape=(3, ))
+input_asks = Input(shape=(3, ))
+input_unit = Input(shape=(1,))
+input_previous_offer=Input(shape=(2, ))
+input_previous_trade=Input(shape=(2, ))
+input_trade_count=Input(shape=(1,))
+input_trade_max=Input(shape=(1,))
+input_trade_min=Input(shape=(1,))
+input_trade_avg=Input(shape=(1,))
+input_trade_mid=Input(shape=(1,))
+input_offer_count=Input(shape=(1,))
+input_offer_max=Input(shape=(1,))
+input_offer_min=Input(shape=(1,))
+input_offer_avg=Input(shape=(1,))
+input_offer_mid=Input(shape=(1,))
+input_role=Input(shape=(1,))
+
+masking_layer = Masking(mask_value=-3)
+
+masked_input_history = masking_layer(input_history)
+masked_input_bids = masking_layer(input_bids)
+masked_input_asks = masking_layer(input_asks)
+masked_input_previous_offer=masking_layer(input_previous_offer)
+masked_input_previous_trade=masking_layer(input_previous_trade)
+
+concatenated = Concatenate()([input_history, masked_input_previous_offer,masked_input_previous_trade, input_unit, masked_input_bids,masked_input_asks, input_trade_count,input_trade_max,input_trade_min,input_trade_avg,input_trade_mid,
+                               input_offer_count,input_offer_max,input_offer_min,input_offer_avg,input_offer_mid,input_role])
+dense1 = Dense(32, activation='relu')(concatenated)
+#dense1=BatchNormalization()(dense1)
+dense2 = Dense(16, activation='relu')(dense1)
+#dense2=BatchNormalization()(dense2)
+dense3 = Dense(8, activation='relu')(dense2)
+regression_output = Dense(1, activation='linear', name='regression_output')(dense3)
+
+
+model = tf.keras.Model(inputs=[input_history, input_bids, input_asks, input_unit,input_previous_offer,
+                               input_previous_trade,input_trade_count,input_trade_max,
+                               input_trade_min,input_trade_avg,input_trade_mid,
+                               input_offer_count,input_offer_max,input_offer_min,input_offer_avg,input_offer_mid,
+                               input_role],
+                       outputs=regression_output)
+
+from keras.callbacks import TensorBoard
+tbCallBack = TensorBoard(log_dir='regre_logs',  
+                 histogram_freq=1,  
+                 write_graph=True,  
+                 write_grads=True, 
+                 write_images=True,
+                 embeddings_freq=0,
+                 embeddings_layer_names=None,
+                 embeddings_metadata=None)
+
+model.compile(optimizer=Adam(lr=0.0005), loss='mean_squared_error')
+model.fit([X_train_history, X_train_bids, X_train_asks, X_train_unit, X_train_previous_offer,X_train_previous_trade,
+           X_train_trade_count,X_train_trade_max,X_train_trade_min,X_train_trade_avg,
+           X_train_trade_mid,X_train_offer_count,X_train_offer_max,X_train_offer_min,
+           X_train_offer_avg,X_train_offer_mid,X_train_role],  y_regression_train, epochs=100, batch_size=128,
+          callbacks=[tbCallBack],
+          validation_data=([X_val_history, X_val_bids, X_val_asks, X_val_unit,X_val_previous_offer,
+                            X_val_previous_trade,X_val_trade_count,X_val_trade_max,
+                            X_val_trade_min,X_val_trade_avg,X_val_trade_mid,X_val_offer_count,
+                            X_val_offer_max,X_val_offer_min,X_val_offer_avg,X_val_offer_mid,X_val_role],
+                            y_regression_val))
+model.evaluate([X_test_history, X_test_bids, X_test_asks, X_test_unit,X_test_previous_offer,
+                X_test_previous_trade,X_test_trade_count,X_test_trade_max,X_test_trade_min,
+                X_test_trade_avg,X_test_trade_mid,X_test_offer_count,X_test_offer_max,
+                X_test_offer_min,X_test_offer_avg,X_test_offer_mid,X_test_role],  y_regression_test)
+
+model.save('../saved_model/regression_model_new.h5')
+
+from keras.models import load_model
+#model = load_model('../saved_model/regression_model.h5')
+y_pred1=model.predict([X_test_history, X_test_bids, X_test_asks, X_test_unit,X_test_previous_offer,
+                       X_test_previous_trade,X_test_trade_count,X_test_trade_max,
+                       X_test_trade_min,X_test_trade_avg,X_test_trade_mid,X_test_offer_count,
+                       X_test_offer_max,X_test_offer_min,X_test_offer_avg,X_test_offer_mid,X_test_role])
+y_regression_test = np.array(y_regression_test)
+y_pred1 = np.array(y_pred1)
+
+
+x_axis = np.arange(len(y_regression_test))
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 6))
+
+plt.plot(x_axis, y_regression_test, label='True Values', color='blue')
+
+plt.plot(x_axis, y_pred1, label='Predicted Values', color='red', linestyle='--')
+
+plt.legend()
+
+plt.title('True vs Predicted Values')
+plt.xlabel('Sample Index')
+plt.ylabel('Value')
+
+plt.show()
+
+# plt.hist(y_regression_test, bins=20, edgecolor='black', density=True)
+# plt.title('offer_hist')
+# plt.xlabel('offer')
+# plt.ylabel('Probability density')
+# plt.show()
+#
+# plt.hist(y_pred1_, bins=20, edgecolor='black', density=True)
+# plt.title('pred_hist')
+# plt.xlabel('offer')
+# plt.ylabel('Probability density')
+# plt.show()
+
---
+++ training/utils.py
@@ -0,0 +1,243 @@
+import heapq
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import keras.backend as K
+import tensorflow as tf
+import os
+import pandas as pd
+
+def padding(name,length,data):
+    padded=[]
+    for entry in data:
+        if name in ['history','previous_offer','previous_trade']:
+            temp=[]
+            for h in entry[name]:
+                if h==18401:
+                    #temp.append(310)
+                    temp.append(-3)
+                elif h==18402:
+                    temp.append(311)
+                else:
+                    temp.append(h)
+            if len(temp)<=length:
+                new = [-3] * (length - len(temp))
+                padded.append(new+temp)
+            else:
+                padded.append(temp[-length:])
+
+        elif name=='bids':
+            key_list = list(entry[name].values())
+            key_list.sort()
+            if len(key_list)<length:
+                new = [-3] * (length - len(key_list))
+                padded.append(key_list+ new)
+            elif len(key_list)>length:
+                padded.append(key_list[-3:])
+            else:
+                padded.append(key_list)
+        elif name=='asks':
+            key_list = list(entry[name].values())
+            key_list.sort()
+            if len(key_list) < length:
+                new = [-3] * (length - len(key_list))
+                padded.append(key_list + new)
+            elif len(key_list) > length:
+                padded.append(key_list[0:3])
+            else:
+                padded.append(key_list)
+    return padded
+
+
+def replace_padding_with_nan(data):
+    mask = (data == -3)
+    data[mask] = np.nan
+    return data
+
+
+def standardize_with_nan(data, scaler, fit=False):
+    mask = ~np.isnan(data)
+    if fit:
+        scaler.fit(data[mask].reshape(-1, 1))
+    data[mask] = scaler.transform(data[mask].reshape(-1, 1)).reshape(-1)
+    return data
+
+
+def replace_nan_with_padding(data):
+    mask = np.isnan(data)
+    data[mask] = -3
+    return data
+
+def generate_data(name,train_data,val_data,test_data):
+    scaler = StandardScaler()
+    ###########################################################################################
+    mean_path = os.path.join('../saved_model', f'{name}_mean.npy')
+    std_path = os.path.join('../saved_model', f'{name}_std.npy')
+
+    if name in ['history','bids','asks','previous_offer','previous_trade']:
+        if name=='history':
+            length=5
+        elif name in ['bids','asks']:
+            length=3
+        else:
+            length=2
+        X_train=padding(name,length,train_data)
+        X_val=padding(name,length,val_data)
+        X_test=padding(name,length,test_data)
+
+        X_train = np.array(X_train, dtype=float)
+        X_val = np.array(X_val, dtype=float)
+        X_test = np.array(X_test, dtype=float)
+        #print(X_train[:10])
+        X_train = replace_padding_with_nan(X_train)
+        X_val = replace_padding_with_nan(X_val)
+        X_test = replace_padding_with_nan(X_test)
+        #print(X_train[:10])
+
+
+        if os.path.exists(mean_path) and os.path.exists(std_path):
+            print('existing')
+
+            mean = np.load(mean_path)
+            std = np.load(std_path)
+            scaler.mean_ = mean
+            scaler.scale_ = std
+            scaler.var_ = std ** 2
+            X_train = standardize_with_nan(X_train, scaler)
+        else:
+            X_train = standardize_with_nan(X_train, scaler, fit=True)
+            # np.save('saved_model/' + name + '_mean.npy', mean)
+            # np.save('saved_model/' + name + '_std.npy', std)
+        X_val = standardize_with_nan(X_val, scaler)
+        X_test = standardize_with_nan(X_test, scaler)
+        #print(X_train[:10])
+        X_train = replace_nan_with_padding(X_train)
+        X_val = replace_nan_with_padding(X_val)
+        X_test = replace_nan_with_padding(X_test)
+        #print(X_train[:10])
+        mean = scaler.mean_
+        std = scaler.scale_
+        print(name)
+        print(mean)
+        print(std)
+
+    elif name in ['time','eq_price','eq_quantity','optimalconsumersurplus','optimalproducersurplus','optimal_surplus','Number_players','Quantity','ConsumerSurplus','ProducerSurplus','surplus','efficiency']:
+
+        market_stat_data = pd.read_csv('../data_processing/market-stat-data.csv')
+        market_stat_data['MarketID_period'] = market_stat_data['MarketID_period'].astype(str)
+
+        train_market_period = [entry['MarketID_period'] for entry in train_data]
+        val_market_period = [entry['MarketID_period'] for entry in val_data]
+        test_market_period = [entry['MarketID_period'] for entry in test_data]
+        if name == 'time':
+
+            market_stat_dict = dict(zip(market_stat_data['MarketID_period'], market_stat_data['offer_time']))
+
+            X_train = [entry[name] / market_stat_dict[train_market_period[i]] for i, entry in enumerate(train_data)]
+            X_val = [entry[name] / market_stat_dict[val_market_period[i]] for i, entry in enumerate(val_data)]
+            X_test = [entry[name] / market_stat_dict[test_market_period[i]] for i, entry in enumerate(test_data)]
+        else:
+
+            market_stat_dict = dict(zip(market_stat_data['MarketID_period'], market_stat_data[name]))
+
+            X_train = [market_stat_dict[train_market_period[i]] for i, entry in enumerate(train_data)]
+            X_val = [market_stat_dict[val_market_period[i]] for i, entry in enumerate(val_data)]
+            X_test = [market_stat_dict[test_market_period[i]] for i, entry in enumerate(test_data)]
+
+            scaler = StandardScaler()
+            X_train = np.array(X_train).reshape(-1, 1)
+            X_train = scaler.fit_transform(X_train).flatten()
+            X_val = np.array(X_val).reshape(-1, 1)
+            X_val = scaler.transform(X_val).flatten()
+            X_test = np.array(X_test).reshape(-1, 1)
+            X_test = scaler.transform(X_test).flatten()
+            mean = scaler.mean_
+            std = scaler.scale_
+            print(name)
+            print(mean)
+            print(std)
+        X_train = tf.convert_to_tensor(X_train, dtype=tf.float32)
+        X_val = tf.convert_to_tensor(X_val, dtype=tf.float32)
+        X_test = tf.convert_to_tensor(X_test, dtype=tf.float32)
+
+    else:
+        if name=='role':
+            X_train = [1 if entry[name] == 'seller' else 0 for entry in train_data]
+            X_val = [1 if entry[name] == 'seller' else 0 for entry in val_data]
+            X_test = [1 if entry[name] == 'seller' else 0 for entry in test_data]
+        else:
+            print(name)
+            X_train = [entry[name] for entry in train_data]
+            X_val = [entry[name] for entry in val_data]
+            X_test = [entry[name] for entry in test_data]
+
+            if name in['behavior','class','class_price','unit','sample_cost_value','reward']:
+                X_train = np.array(X_train)
+                X_val = np.array(X_val)
+                X_test = np.array(X_test)
+                ###########################################################################################
+                if name=='behavior':
+                    X_train = X_train // 10
+                    X_val = X_val // 10
+                    X_test = X_test// 10
+                if name=='sample_cost_value' or name=='reward':
+                    X_train = X_train / 150
+                    X_val = X_val / 150
+                    X_test = X_test / 150
+            else:
+
+                if os.path.exists(mean_path) and os.path.exists(std_path):
+                    print('existing')
+
+                    mean = np.load(mean_path)
+                    std = np.load(std_path)
+                    scaler.mean_ = mean
+                    scaler.scale_ = std
+                    scaler.var_ = std ** 2
+
+                    X_train = np.array(X_train).reshape(-1, 1)
+                    X_train = scaler.transform(X_train)
+                else:
+
+                    X_train = np.array(X_train).reshape(-1, 1)
+                    X_train = scaler.fit_transform(X_train)
+                    # np.save('saved_model/' + name + '_mean.npy', mean)
+                    # np.save('saved_model/' + name + '_std.npy', std)
+                X_val = np.array(X_val).reshape(-1, 1)
+                X_val = scaler.transform(X_val)
+                X_test = np.array(X_test).reshape(-1, 1)
+                X_test = scaler.transform(X_test)
+                mean = scaler.mean_
+                std = scaler.scale_
+                print(name)
+                print(mean)
+                print(std)
+        X_train = tf.convert_to_tensor(X_train, dtype=tf.float32)
+        X_val = tf.convert_to_tensor(X_val, dtype=tf.float32)
+        X_test = tf.convert_to_tensor(X_test, dtype=tf.float32)
+    return X_train,X_val,X_test
+
+def F1_score(y_true, y_pred):
+    # Count positive samples.
+    c1 = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    c2 = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    c3 = K.sum(K.round(K.clip(y_true, 0, 1)))
+    # If there are no true samples, fix the F1 score at 0.
+    if c3 == 0.0:
+        return K.cast(0, K.floatx())
+    # How many selected items are relevant?
+    precision = c1 / (c2 + K.epsilon())
+    #tf.print("Precision:", precision)
+    # How many relevant items are selected?
+    recall = c1 / (c3 + K.epsilon())
+    #tf.print("Recall:", recall)
+    # Calculate f1_score
+    f1_score = 2 * (precision * recall) / (precision + recall+ K.epsilon())
+    return f1_score
+
+def negative_log_likelihood(y_true, y_pred):
+    mu = y_pred[0]
+    std = y_pred[1]
+    mu = tf.cast(mu, tf.float32)
+    std = tf.cast(std, tf.float32)
+    y_true = tf.cast(y_true, tf.float32)
+    return -tf.reduce_mean(-0.5 * tf.math.log(2 * np.pi * tf.pow(std, 2))- tf.pow(y_true - mu, 2) / (2 * tf.pow(std, 2)))
